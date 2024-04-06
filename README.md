f3e154eb50ae8a092df7bd2a31efed4848e8e718
sahme73 <sahme73@illinois.edu>
2024-04-06 05:28:03 -0400

    setup lsp for cpp, java, and ts

--- README.md
+++ README.md
@@ -11,4 +11,10 @@
 1. `dnf install ripgrep`
 2. `dnf install fd-find`
 3. `dnf install diffutils`
-4. ...
+4. `dnf install unzip`
+5. `dnf install java-21-openjdk-jmods`
+6. `dnf install java-latest-openjdk-devel.x86_64`
+7. `dnf install java`
+8. `dnf install maven`
+9. `dnf install npm`
+10. `npx ts-node`
---
+++ after/plugin/lspzero.lua
@@ -0,0 +1,30 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+local cmp = require("cmp")
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+    sign_icons = { }
+})
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = {},
+    handlers = {
+        lsp.default_setup,
+    },
+})
